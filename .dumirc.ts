759fc0f6e593746f57f37f32a342a8fecf78da51
chenhaibin <chenhaibin07@meituan.com>
2025-06-18 20:10:34 +0800

    组件库迁移，之前是IceCream-UI

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/cube/',
+  publicPath: '/cube/',
   outputPath: 'docs-dist',
   favicons: ['https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png'],
   themeConfig: {
